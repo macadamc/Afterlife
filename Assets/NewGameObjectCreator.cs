a7df0f540f7a669c2bdfd7700cb3ad6a58d1a318
chris macadam <macadamchris@gmail.com>
2019-01-25 14:01:25 -0500

    art and prefab changes

---
+++ Assets/NewGameObjectCreator.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Sirenix.OdinInspector.Editor;
+using Sirenix.OdinInspector;
+using Sirenix.Utilities.Editor;
+using UnityEngine.Rendering;
+
+public class NewGameObjectCreator : OdinEditorWindow
+{
+    public enum ColliderType {Box, Circle};
+
+    [Space(10)]
+    [Title("Create New GameObject")]
+    public string GameObjectName = "GameObject";
+    [PreviewField]
+    public Sprite baseSprite;
+    public bool useZSorting;
+    public ColliderType colliderType;
+    public bool isTrigger;
+
+    [MenuItem("Tools/ShadyPixel/New GameObject Creator")]
+    private static void OpenWindow()
+    {
+        var window = GetWindow<NewGameObjectCreator>();
+        window.Show();
+        window.titleContent = new GUIContent("New GameObject", EditorIcons.Male.Active);
+    }
+
+    [Button("Create Object", ButtonSizes.Medium)]
+    private void CreateObject()
+    {
+        if (EditorApplication.isPlaying)
+        {
+            Debug.LogWarning("You are creating a GameObject in play mode. Any object created in play mode, will not be saved upon exiting.");
+        }
+
+        // setting up base object
+        GameObject baseObject = new GameObject(GameObjectName);
+
+        // setting up sprite renderer
+        SpriteRenderer spriteObject = new GameObject("Sprite").AddComponent<SpriteRenderer>();
+        spriteObject.sprite = baseSprite;
+        if (useZSorting)
+        {
+            spriteObject.gameObject.AddComponent<SortingGroup>();
+            spriteObject.gameObject.AddComponent<UpdateZOrder>();
+            SortingGroup sortingGroup = spriteObject.GetComponent<SortingGroup>();
+            sortingGroup.sortingLayerName = "Entity";
+        }
+        spriteObject.transform.parent = baseObject.transform;
+
+        // setting up collider
+        GameObject colliderObject = new GameObject("Collider");
+        Collider2D col;
+        if(colliderType == ColliderType.Box)
+        {
+            col = colliderObject.AddComponent<BoxCollider2D>();
+        }
+        else
+        {
+            col =colliderObject.AddComponent<CircleCollider2D>();
+        }
+        col.isTrigger = isTrigger;
+        colliderObject.transform.parent = baseObject.transform;
+
+
+        // closing the window
+        EditorApplication.delayCall += Close;
+    }
+}
---
+++ Assets/NewSceneCreator.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Sirenix.OdinInspector.Editor;
+using Sirenix.OdinInspector;
+using Sirenix.Utilities.Editor;
+
+public class NewSceneCreator : OdinEditorWindow
+{
+    [Space(10)]
+    [Title("Create New Scene")]
+    public string NewSceneName = "NewScene";
+    public bool AddToBuildSettings = true;
+    [Space(10)]
+    [InfoBox("Optional. Will use default values if left blank.")]
+    public string AreaName = "";
+    public AmbientLightSettings ambientLightSettings;
+    public AudioClip BgmClip;
+
+    [MenuItem("Tools/ShadyPixel/New Scene Creator")]
+    private static void OpenWindow()
+    {
+        var window = GetWindow<NewSceneCreator>();
+        window.Show();
+        window.titleContent = new GUIContent("New Scene", EditorIcons.StarPointer.Active);
+    }
+
+    [Button("Create Scene", ButtonSizes.Medium)]
+    public void CheckAndCreateScene()
+    {
+        if (EditorApplication.isPlaying)
+        {
+            Debug.LogWarning("Cannot create scene while in play mode. Exit play mode first.");
+            return;
+        }
+
+        Scene currentActiveScene = SceneManager.GetActiveScene();
+        if (currentActiveScene.isDirty)
+        {
+            string title = currentActiveScene.name + " Has Been Modified";
+            string message = "Do you want to save the changes you made to " + currentActiveScene.path + "?\nChanges will be lost if you don't save them.";
+            int option = EditorUtility.DisplayDialogComplex(title, message, "Save", "Don't Save", "Cancel");
+
+            if (option == 0)
+            {
+                EditorSceneManager.SaveScene(currentActiveScene);
+            }
+            else if (option == 2)
+            {
+                return;
+            }
+        }
+
+        CreateScene();
+    }
+
+    public void CreateScene()
+    {
+        string[] result = AssetDatabase.FindAssets("_TemplateScene");
+
+        if (result.Length > 0)
+        {
+            string newScenePath = "Assets/" + NewSceneName + ".unity";
+            AssetDatabase.CopyAsset(AssetDatabase.GUIDToAssetPath(result[0]), newScenePath);
+            AssetDatabase.Refresh();
+            Scene newScene = EditorSceneManager.OpenScene(newScenePath, OpenSceneMode.Single);
+            if(AddToBuildSettings)
+                AddSceneToBuildSettings(newScene);
+
+            SetUpScene();
+
+            Debug.Log("Created and Opened :" + newScenePath + ".");
+            //have to use delay call because bug with odin, just waits until end of frame I think
+            EditorApplication.delayCall += Close;
+        }
+        else
+        {
+            //Debug.LogError("The template scene <b>_TemplateScene</b> couldn't be found ");
+            EditorUtility.DisplayDialog("Error",
+                "The scene _TemplateScene was not found in Assets/Scenes folder. This scene is required by the New Scene Creator.",
+                "OK");
+        }
+    }
+
+    public void AddSceneToBuildSettings(Scene scene)
+    {
+        EditorBuildSettingsScene[] buildScenes = EditorBuildSettings.scenes;
+
+        EditorBuildSettingsScene[] newBuildScenes = new EditorBuildSettingsScene[buildScenes.Length + 1];
+        for (int i = 0; i < buildScenes.Length; i++)
+        {
+            newBuildScenes[i] = buildScenes[i];
+        }
+        newBuildScenes[buildScenes.Length] = new EditorBuildSettingsScene(scene.path, true);
+        EditorBuildSettings.scenes = newBuildScenes;
+    }
+
+    public void SetUpScene()
+    {
+        if(AreaName.Length > 0)
+        {
+            FindObjectOfType<SetAreaName>().areaName = AreaName;
+        }
+
+        if(BgmClip != null)
+        {
+            FindObjectOfType<PlayBGM>().clip = BgmClip;
+        }
+
+        if(ambientLightSettings != null)
+        {
+            var ambientLight = FindObjectOfType<SetAmbientLight>();
+            ambientLight.settings = ambientLightSettings;
+            ambientLight.UpdateLight();
+        }
+    }
+}
